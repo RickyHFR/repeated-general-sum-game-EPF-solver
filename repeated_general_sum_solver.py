3e466e0fd9609b70243277c925a92fda6a2b986e
RickyHFR <huangfengr@gmail.com>
2025-03-16 13:28:02 +0800

    Attempt iterated version

--- repeated_general_sum_solver.py
+++ repeated_general_sum_solver.py
@@ -1,3 +1,4 @@
+from typing import Union
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
@@ -7,10 +8,7 @@ class EPF:
         self.knots = knots
 
     def left_truncate(self, threshold):
-        print(threshold)
-        print(self.knots)
         idx = np.searchsorted(self.knots[:, 0], threshold, side='left')
-        print(idx)
         if idx == 0:
             return self
         elif idx == self.knots.shape[0]:
@@ -48,6 +46,22 @@ class EPF:
             upper_hull.append(p)
         
         return EPF(np.array(upper_hull))
+    
+    def shift_and_scale(self, shift: tuple[float, float], scale: float):
+        if self.knots is None:
+            return self
+        shift_vector = np.array(shift)
+        return EPF(self.knots * scale + shift_vector)
+    
+    def get_leader_payoff(self, follower_payoff):
+        if self.knots is None:
+            return -np.inf
+        idx = np.searchsorted(self.knots[:, 1], follower_payoff, side='left')
+        if idx == 0 or idx == self.knots.shape[0]:
+            return -np.inf
+        x1, y1 = self.knots[idx - 1]
+        x2, y2 = self.knots[idx]
+        return x1 + (x2 - x1) / (y2 - y1) * (follower_payoff - y1)
     
     def min_follower_payoff(self):
         return self.knots[0, 0]
@@ -104,6 +118,52 @@ class GameNode:
         print(EPF.knots)
         plt.plot(EPF.knots[:, 0], EPF.knots[:, 1])
 
+class ReapeatedGameNode(GameNode):
+    def __init__(self, player: int, payoff: Union[tuple[float, float], EPF] = None, is_root = False, discount_factor: float = 0.9,
+                 max_depth: int = 0):
+        super().__init__(player, payoff, is_root)
+        self.discount_factor = discount_factor
+        self.max_depth = max_depth
+
+    def get_grim_value(self, curr_depth = 0):
+        if self.grim_value is not None:
+            return self.grim_value
+        if self.player == -1 and curr_depth == self.max_depth:
+            self.grim_value = self.payoff[0] * self.discount_factor ** curr_depth
+        elif self.player == -1 and curr_depth < self.max_depth:
+            self.grim_value = self.payoff[0] * self.discount_factor ** curr_depth + self.get_grim_value(curr_depth + 1) * self.discount_factor ** (curr_depth + 1)
+        elif self.player == 0:
+            self.grim_value = min([child.get_grim_value(curr_depth) for child in self.children])
+        else:
+            self.grim_value = max([child.get_grim_value(curr_depth) for child in self.children])
+        return self.grim_value  
+
+    def get_EPF(self, curr_depth = 0):
+        if self.player == -1 and curr_depth == self.max_depth:
+            return EPF(np.array([[self.payoff[0], self.payoff[1]]])).shift_and_scale((0, 0), self.discount_factor ** curr_depth)
+        elif self.player == -1 and curr_depth < self.max_depth:
+            return self.get_EPF(curr_depth + 1).shift_and_scale(
+                (self.payoff[0] * self.discount_factor ** curr_depth, self.payoff[1] * self.discount_factor ** curr_depth),
+                self.discount_factor)
+        if self.player == 0:
+            result_EPF = EPF(None)
+            for child in self.children:
+                result_EPF = result_EPF.find_concave_envelope(child.get_EPF(curr_depth))
+            # print(result_EPF.knots)
+            return result_EPF
+        if self.player == 1:
+            result_EPF = EPF(None)
+            for child in self.children:
+                child_threshold = max([other_child.get_grim_value(curr_depth) for other_child in self.children if other_child != child])
+                result_EPF = result_EPF.find_concave_envelope(child.get_EPF(curr_depth).left_truncate(child_threshold))
+            print (result_EPF.knots)
+            return result_EPF
+        
+    def draw_EPF(self):
+        EPF = self.get_EPF()
+        print(EPF.knots)
+        plt.plot(EPF.knots[:, 0], EPF.knots[:, 1])
+
 
 # define a simple prisoner's dilemma game
 def prisoners_dilemma():
@@ -120,18 +180,16 @@ def prisoners_dilemma():
     c1.add_child(l2)
     c2.add_child(l3)
     c2.add_child(l4)
-
     return root
 
-# define chiken game
-def chicken():
-    root = GameNode(player=1, is_root=True)
-    c1 = GameNode(player=0)
-    c2 = GameNode(player=0)
-    l1 = GameNode(player=-1, payoff=(0, 0))
-    l2 = GameNode(player=-1, payoff=(1, -1))
-    l3 = GameNode(player=-1, payoff=(-1, 1))
-    l4 = GameNode(player=-1, payoff=(-100, -100))
+def iterated_prisoners_dilemma():
+    root = ReapeatedGameNode(player=1, is_root=True)
+    c1 = ReapeatedGameNode(player=0)
+    c2 = ReapeatedGameNode(player=0)
+    l1 = ReapeatedGameNode(player=-1, payoff=(4, 4))
+    l2 = ReapeatedGameNode(player=-1, payoff=(0, 10))
+    l3 = ReapeatedGameNode(player=-1, payoff=(10, 0))
+    l4 = ReapeatedGameNode(player=-1, payoff=(9, 9))
     root.add_child(c1)
     root.add_child(c2)
     c1.add_child(l1)
@@ -140,6 +198,6 @@ def chicken():
     c2.add_child(l4)
     return root
 
-game = chicken()
+game = iterated_prisoners_dilemma()
 game.draw_EPF()
 plt.show()
